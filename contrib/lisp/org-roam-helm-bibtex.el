07730c97b28a995f6db32343c37a65ea073897e4
Leo Vivier <zaephon@gmail.com>
2020-04-12 09:30:44 +0200

    (fix) format conventional header
    Cues taken from ‘org-roam.el’ and doc:
    https://www.gnu.org/software/emacs/manual/html_node/elisp/Library-Headers.html

--- contrib/lisp/org-roam-helm-bibtex.el
+++ contrib/lisp/org-roam-helm-bibtex.el
@@ -1,6 +1,29 @@
-;;; org-roam-helm-bibtex.el --- Org-roam helm-bibtex integration  -*- fill-column: 78; lexical-binding: t; -*-
+;;; org-roam-helm-bibtex.el --- Org-roam helm-bibtex integration -*- coding: utf-8; lexical-binding: t; -*-
 
-;; Authors: Leo Vivier <leo.vivier+dev@gmail.com>
+;; Copyright © 2020 Jethro Kuan <jethrokuan95@gmail.com>
+;; Copyright © 2020 Leo Vivier <leo.vivier+dev@gmail.com>
+
+;; Author: Leo Vivier <leo.vivier+dev@gmail.com>
+;; 	Jethro Kuan <jethrokuan95@gmail.com>
+;; Created: 12 April 2020
+;; Keywords: bibtex, refs
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
 
 ;;; Commentary:
 
@@ -10,7 +33,7 @@
 ;; script creates one.
 
 ;;; Code:
-
+;;;; Library Requires
 (require 'org-roam)
 (require 'bibtex)
 (require 'bibtex-completion)
