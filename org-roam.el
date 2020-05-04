f6fee72cdc2b03263bdea6a15ef8bb44af247ffa
Leo Vivier <leo.vivier+dev@gmail.com>
2020-05-04 13:17:22 +0200

    Improve docstring

--- org-roam.el
+++ org-roam.el
@@ -784,7 +784,10 @@ See `org-roam--get-ref-path-completions' for details."
   :group 'org-roam)
 
 (defun org-roam--get-ref-path-completions (&optional interactive)
-  "Return a list of cons pairs for refs to absolute path of Org-roam files."
+  "Return a list of cons pairs for refs to absolute path of Org-roam files.
+When INTERACTIVE `org-roam-include-type-in-ref-path-completions'
+are non-nil, format the car of the completion-candidates as
+'type:ref'."
   (let ((rows (org-roam-db-query [:select [type ref file] :from refs]))
         (include-type (and interactive
                            org-roam-include-type-in-ref-path-completions)))
