a334cc69af0832b6e00005a08afa70b672c075a5
Leo Vivier <leo.vivier+dev@gmail.com>
2020-05-12 20:43:21 +0200

    Fix another missing capital

--- org-roam-db.el
+++ org-roam-db.el
@@ -1,4 +1,4 @@
-;;; org-roam-db.el --- org-roam database API -*- coding: utf-8; lexical-binding: t; -*-
+;;; org-roam-db.el --- Org-roam database API -*- coding: utf-8; lexical-binding: t; -*-
 
 ;; Copyright © 2020 Jethro Kuan <jethrokuan95@gmail.com>
 
