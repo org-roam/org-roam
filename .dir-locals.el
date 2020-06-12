f631679c925f40598ea566152485bf5725f35ed7
Leo Vivier <leo.vivier+dev@gmail.com>
2020-06-12 13:45:36 +0200

    (fix): Fix local-var loading for dev suite

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,6 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((emacs-lisp-mode . ((eval . (require 'org-roam-dev)))))
+((emacs-lisp-mode
+  (eval . (require 'org-roam-dev))
+  (sentence-end-double-space . nil)))
