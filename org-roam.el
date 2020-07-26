c72a92d82f1aa6d1dbf0706d9b12f3a2e1fd2a36
Leo Vivier <leo.vivier+dev@gmail.com>
2020-07-26 23:51:47 +0200

    Shield for no region

--- org-roam.el
+++ org-roam.el
@@ -1473,8 +1473,9 @@ If DESCRIPTION is provided, use this as the link label.  See
                    (delete-region min max))
                  (insert (org-roam--format-link target-file-path link-description)))
                 (t
-                 (pcase-let ((`(,min . ,max) region))
-                   (add-text-properties min max '(read-only t)))
+                 (when region
+                   (pcase-let ((`(,min . ,max) region))
+                     (add-text-properties min max '(read-only t))))
                  (let ((org-roam-capture--info `((title . ,title-with-tags)
                                                  (slug . ,(funcall org-roam-title-to-slug-function title-with-tags))))
                        (org-roam-capture--context 'title))
