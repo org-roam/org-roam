b0aa03b276bd28468cd968900cca1643276c4503
carlos <huaixian.huang@gmail.com>
2020-09-19 21:09:20 +0800

    fix: typo of straggling a and replace tramp-handle-file-remote-p with file-remote-p
    because this package is no need relate with tramp

--- org-roam.el
+++ org-roam.el
@@ -1541,8 +1541,8 @@ update with NEW-DESC."
       (when-let ((link (org-element-lineage (org-element-context) '(link) t)))
         (let ((type (org-element-property :type link))
               (path (org-element-property :path link)))
-          (when (and (not (tramp-handle-file-remote-p path))
-                     (string-equal (file-truename path) old-path)a
+          (when (and (not (file-remote-p path))
+                     (string-equal (file-truename path) old-path)
                      (org-in-regexp org-link-bracket-re 1))
             (let* ((label (if (match-end 2)
                               (match-string-no-properties 2)
