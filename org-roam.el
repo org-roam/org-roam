5289cad3b31b0a358fa26a41e57ee44ee55b5a64
USAMI Kenta <tadsan@zonu.me>
2021-04-05 03:53:39 +0900

    Add org-roam-slug-trim-chars instead of org-roam-slug--preserve-chars-from-normalization

--- org-roam.el
+++ org-roam.el
@@ -234,12 +234,32 @@ Function should return a filename string based on title."
   :type 'function
   :group 'org-roam)
 
-(defcustom org-roam-slug--preserve-chars-from-normalization
-  '(;; For Japanese charecters
-    12441 ; U+3099 COMBINING KATAKANA-HIRAGANA VOICED SOUND MARK
-    12442 ; U+309A COMBINING KATAKANA-HIRAGANA SEMI-VOICED SOUND MARK
+(defcustom org-roam-slug-trim-chars
+  '(;; Combining Diacritical Marks https://www.unicode.org/charts/PDF/U0300.pdf
+    768 ; U+0300 COMBINING GRAVE ACCENT
+    769 ; U+0301 COMBINING ACUTE ACCENT
+    770 ; U+0302 COMBINING CIRCUMFLEX ACCENT
+    771 ; U+0303 COMBINING TILDE
+    772 ; U+0304 COMBINING MACRON
+    774 ; U+0306 COMBINING BREVE
+    775 ; U+0307 COMBINING DOT ABOVE
+    776 ; U+0308 COMBINING DIAERESIS
+    777 ; U+0309 COMBINING HOOK ABOVE
+    778 ; U+030A COMBINING RING ABOVE
+    780 ; U+030C COMBINING CARON
+    795 ; U+031B COMBINING HORN
+    803 ; U+0323 COMBINING DOT BELOW
+    804 ; U+0324 COMBINING DIAERESIS BELOW
+    805 ; U+0325 COMBINING RING BELOW
+    807 ; U+0327 COMBINING CEDILLA
+    813 ; U+032D COMBINING CIRCUMFLEX ACCENT BELOW
+    814 ; U+032E COMBINING BREVE BELOW
+    816 ; U+0330 COMBINING TILDE BELOW
+    817 ; U+0331 COMBINING MACRON BELOW
     )
-  "Characters to preserve from Unicode normalization for slug."
+  "Characters to trim from Unicode normalization for slug.
+
+By default, the characters are specified to remove Diacritical Marks from the Latin alphabet."
   :type '(repeat character)
   :group 'org-roam)
 
@@ -803,8 +823,7 @@ Each ref is returned as a cons of its type and its key."
 (defun org-roam--title-to-slug (title)
   "Convert TITLE to a filename-suitable slug."
   (cl-flet* ((nonspacing-mark-p (char)
-                                (and (eq 'Mn (get-char-code-property char 'general-category))
-                                     (not (memq char org-roam-slug-preserve-chars-from-normalization))))
+                                (memq char org-roam-slug-trim-chars))
              (strip-nonspacing-marks (s)
                                      (ucs-normalize-NFC-string
                                       (apply #'string (seq-remove #'nonspacing-mark-p
