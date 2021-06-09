6b3c2cf8d81442e3bfeaaec505e98b2bd2f3498a
Jethro Kuan <jethrokuan95@gmail.com>
2021-06-09 19:07:33 +0800

    move slug-trim-chars into org-roam-node-slug

--- org-roam.el
+++ org-roam.el
@@ -133,36 +133,6 @@ method symbol as a cons cell. For example: '(find (rg . \"/path/to/rg\"))."
   :type '(set (const :tag "find" find)
               (const :tag "rg" rg)))
 
-(defcustom org-roam-slug-trim-chars
-  '(;; Combining Diacritical Marks https://www.unicode.org/charts/PDF/U0300.pdf
-    768 ; U+0300 COMBINING GRAVE ACCENT
-    769 ; U+0301 COMBINING ACUTE ACCENT
-    770 ; U+0302 COMBINING CIRCUMFLEX ACCENT
-    771 ; U+0303 COMBINING TILDE
-    772 ; U+0304 COMBINING MACRON
-    774 ; U+0306 COMBINING BREVE
-    775 ; U+0307 COMBINING DOT ABOVE
-    776 ; U+0308 COMBINING DIAERESIS
-    777 ; U+0309 COMBINING HOOK ABOVE
-    778 ; U+030A COMBINING RING ABOVE
-    780 ; U+030C COMBINING CARON
-    795 ; U+031B COMBINING HORN
-    803 ; U+0323 COMBINING DOT BELOW
-    804 ; U+0324 COMBINING DIAERESIS BELOW
-    805 ; U+0325 COMBINING RING BELOW
-    807 ; U+0327 COMBINING CEDILLA
-    813 ; U+032D COMBINING CIRCUMFLEX ACCENT BELOW
-    814 ; U+032E COMBINING BREVE BELOW
-    816 ; U+0330 COMBINING TILDE BELOW
-    817 ; U+0331 COMBINING MACRON BELOW
-    )
-  "Characters to trim from Unicode normalization for slug.
-
-By default, the characters are specified to remove Diacritical
-Marks from the Latin alphabet."
-  :type '(repeat character)
-  :group 'org-roam)
-
 ;;;; ID Utilities
 (defun org-roam-id-at-point ()
   "Return the ID at point, if any.
@@ -453,9 +423,31 @@ OLD-FILE is cleared from the database, and NEW-FILE-OR-DIR is added."
 
 (cl-defmethod org-roam-node-slug ((node org-roam-node))
   "Return the slug of NODE."
-  (let ((title (org-roam-node-title node)))
+  (let ((title (org-roam-node-title node))
+        (slug-trim-chars '(;; Combining Diacritical Marks https://www.unicode.org/charts/PDF/U0300.pdf
+                           768 ; U+0300 COMBINING GRAVE ACCENT
+                           769 ; U+0301 COMBINING ACUTE ACCENT
+                           770 ; U+0302 COMBINING CIRCUMFLEX ACCENT
+                           771 ; U+0303 COMBINING TILDE
+                           772 ; U+0304 COMBINING MACRON
+                           774 ; U+0306 COMBINING BREVE
+                           775 ; U+0307 COMBINING DOT ABOVE
+                           776 ; U+0308 COMBINING DIAERESIS
+                           777 ; U+0309 COMBINING HOOK ABOVE
+                           778 ; U+030A COMBINING RING ABOVE
+                           780 ; U+030C COMBINING CARON
+                           795 ; U+031B COMBINING HORN
+                           803 ; U+0323 COMBINING DOT BELOW
+                           804 ; U+0324 COMBINING DIAERESIS BELOW
+                           805 ; U+0325 COMBINING RING BELOW
+                           807 ; U+0327 COMBINING CEDILLA
+                           813 ; U+032D COMBINING CIRCUMFLEX ACCENT BELOW
+                           814 ; U+032E COMBINING BREVE BELOW
+                           816 ; U+0330 COMBINING TILDE BELOW
+                           817 ; U+0331 COMBINING MACRON BELOW
+                           )))
     (cl-flet* ((nonspacing-mark-p (char)
-                                  (memq char org-roam-slug-trim-chars))
+                                  (memq char slug-trim-chars))
                (strip-nonspacing-marks (s)
                                        (ucs-normalize-NFC-string
                                         (apply #'string (seq-remove #'nonspacing-mark-p
