8f7f0e8dd11a097657a848931a059c3c5279c582
Jethro Kuan <jethrokuan95@gmail.com>
2021-06-17 20:42:58 +0800

    move org-roam-refile into org-roam.el

--- org-roam.el
+++ org-roam.el
@@ -1062,5 +1062,70 @@ To be added to `org-open-at-point-functions'."
 
 (add-hook 'org-roam-find-file-hook #'org-roam-open-id-with-org-roam-db-h)
 
+;;; Refiling
+(defun org-roam-demote-entire-buffer ()
+  "Convert an org buffer with any top level content to a single node.
+
+All headings are demoted one level.
+
+The #+TITLE: keyword is converted into a level-1 heading and deleted.
+Any tags declared on #+FILETAGS: are transferred to tags on the new top heading.
+
+Any top level properties drawers are incorporated into the new heading."
+  (interactive)
+  (org-with-point-at 1
+    (org-map-entries 'org-do-demote)
+    (insert "* "
+            (org-roam--file-keyword-get "TITLE")
+            "\n")
+    (org-back-to-heading)
+    (org-set-tags (org-roam--file-keyword-get "FILETAGS"))
+    (org-roam--file-keyword-kill "TITLE")
+    (org-roam--file-keyword-kill "FILETAGS")))
+
+(defun org-roam-refile ()
+  "Refile to node."
+  (interactive)
+  (let* ((regionp (org-region-active-p))
+         (region-start (and regionp (region-beginning)))
+         (region-end (and regionp (region-end)))
+         (node (org-roam-node-read nil nil nil 'require-match))
+         (file (org-roam-node-file node))
+         (nbuf (or (find-buffer-visiting file)
+                   (find-file-noselect file)))
+         level reversed)
+    (if regionp
+        (progn
+          (org-kill-new (buffer-substring region-start region-end))
+          (org-save-markers-in-region region-start region-end))
+      (progn
+        (if (org-before-first-heading-p)
+            (org-roam-demote-entire-buffer))
+        (org-copy-subtree 1 nil t)))
+    (with-current-buffer nbuf
+      (org-with-wide-buffer
+       (goto-char (org-roam-node-point node))
+       (setq level (org-get-valid-level (funcall outline-level) 1)
+             reversed (org-notes-order-reversed-p))
+       (goto-char
+        (if reversed
+            (or (outline-next-heading) (point-max))
+          (or (save-excursion (org-get-next-sibling))
+              (org-end-of-subtree t t)
+              (point-max))))
+       (unless (bolp) (newline))
+       (org-paste-subtree level nil nil t)
+       (and org-auto-align-tags
+            (let ((org-loop-over-headlines-in-active-region nil))
+              (org-align-tags)))
+       (when (fboundp 'deactivate-mark) (deactivate-mark))))
+    (if regionp
+        (delete-region (point) (+ (point) (- region-end region-start)))
+      (org-preserve-local-variables
+       (delete-region
+        (and (org-back-to-heading t) (point))
+        (min (1+ (buffer-size)) (org-end-of-subtree t t) (point)))))
+    (org-roam--kill-empty-buffer)))
+
 (provide 'org-roam)
 ;;; org-roam.el ends here
