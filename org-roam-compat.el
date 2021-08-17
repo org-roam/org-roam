aac41a22e486c6473ec92867f755efeacf4a0610
Wetlize <wetlize@gmail.com>
2021-08-17 20:53:06 +0300

    update org-roam-compat.el

--- org-roam-compat.el
+++ org-roam-compat.el
@@ -163,6 +163,9 @@ nodes." org-id-locations-file)
 (define-obsolete-function-alias
   'org-roam-teardown
   'org-roam-db-autosync-disable "org-roam 2.0")
+(define-obsolete-variable-alias
+  'org-roam-db-update-on-save
+  'org-roam-db-autosync-update-method "org-roam 2.0")
 
 (define-obsolete-variable-alias
   'org-roam-current-node
@@ -170,9 +173,6 @@ nodes." org-id-locations-file)
 (define-obsolete-variable-alias
   'org-roam-current-directory
   'org-roam-buffer-current-directory "org-roam 2.0")
-(define-obsolete-variable-alias
-  'org-roam-db-update-on-save
-  'org-roam-db-update-method "org-roam 2.0")
 (define-obsolete-function-alias
   'org-roam-buffer-render
   'org-roam-buffer-render-contents "org-roam 2.0")
