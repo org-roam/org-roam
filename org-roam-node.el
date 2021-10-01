org-roam/org-roam

commit d2fc4be73b99a21add1a209691218dcd8a3c59e9

optimize(core) linear->constant time filter-fn (#1876)
in org-roam-node-read, there is no need to check filter-fn for each node, move the branch up, O(n) to O(1)

org-roam-node.el
@@ -441,8 +441,10 @@ SORT-FN is a function to sort nodes. See `org-roam-node-read-sort-by-file-mtime'
 for an example sort function.
 If REQUIRE-MATCH, the minibuffer prompt will require a match."
   (let* ((nodes (org-roam-node-read--completions))
-         (nodes (cl-remove-if-not (lambda (n)
-                                    (if filter-fn (funcall filter-fn (cdr n)) t)) nodes))
+         (nodes (if filter-fn
+                    (cl-remove-if-not
+                     (lambda (n) (funcall filter-fn (cdr n)) t))
+                  nodes))
          (sort-fn (or sort-fn
                       (when org-roam-node-default-sort
                         (intern (concat "org-roam-node-read-sort-by-"

commit dbfbe6fdc526142e3283ab879c5cd698ea5dec19

fix(org-roam-node-read): fix regression introduced in #1876
Fix `filter-fn` not working like its supposed to.

org-roam-node.el
@@ -443,7 +443,8 @@ If REQUIRE-MATCH, the minibuffer prompt will require a match."
   (let* ((nodes (org-roam-node-read--completions))
          (nodes (if filter-fn
                     (cl-remove-if-not
-                     (lambda (n) (funcall filter-fn (cdr n)) t))
+                     (lambda (n) (funcall filter-fn (cdr n)))
+                     nodes)
                   nodes))
          (sort-fn (or sort-fn
                       (when org-roam-node-default-sort