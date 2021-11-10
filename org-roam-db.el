7db6fe36312acb42f112bae9441886eca4ab3d5b
Philippe Crama <pcfeb0009@gmx.com>
2021-11-10 22:06:46 +0100

    Drive-by fix for org-roam-database-connector's :group

--- org-roam-db.el
+++ org-roam-db.el
@@ -48,7 +48,7 @@ tool, which is not recommended because it is not suitable to be
 used like this, but has the advantage that you likely don't need
 a compiler. See https://nullprogram.com/blog/2014/02/06/."
   :package-version '(org-roam . "2.2.0")
-  :group 'forge
+  :group 'org-roam
   :type '(choice (const sqlite)
                  (const libsqlite3)
                  (const sqlite3)
