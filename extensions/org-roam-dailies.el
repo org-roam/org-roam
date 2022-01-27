7ef2c359cbbda1bd4237256db525aead374374ff
Michael Eliachevitch <m.eliachevitch@posteo.de>
2022-01-27 14:14:23 +0100

    Allow specifying keys in all org-roam-dailies capture/goto arguments
    This is an extension of PR #2028 [1] by @astery, where the `keys`
    argument was added to `org-roam-dailies--capture` `org-roam-dailies-capture-today`.
    
    I extended this argument to also be available in other built-in dailies
    functions, as I didn't see any reason why not and it results in my opinion in
    more consistent behaviour.
    
    The rationale is the same as in #2028: Allow users to add keybindings (or in my
    case hydras) to calls of org-roam-dailies functions with specific template keys,
    thus circumventing the selection screen.
    
    So far it seems to work for me. I have only one pet-peeve about
    the interface, if the user starts adding keys to all their goto-functions, they
    might add
    
          (org-roam-dailies-goto-date nil "<key>")
    
    Then the key-string will be interpreted as the value for `prefer-future` and
    since it's non-nil, there won't be an error.
    
    [1] https://github.com/org-roam/org-roam/pull/2028

--- extensions/org-roam-dailies.el
+++ extensions/org-roam-dailies.el
@@ -139,14 +139,14 @@ In this case, interactive selection will be bypassed."
   (org-roam-dailies--capture (current-time) goto keys))
 
 ;;;###autoload
-(defun org-roam-dailies-goto-today ()
+(defun org-roam-dailies-goto-today (keys)
   "Find the daily-note for today, creating it if necessary."
   (interactive)
-  (org-roam-dailies-capture-today t))
+  (org-roam-dailies-capture-today t keys))
 
 ;;;; Tomorrow
 ;;;###autoload
-(defun org-roam-dailies-capture-tomorrow (n &optional goto)
+(defun org-roam-dailies-capture-tomorrow (n &optional goto keys)
   "Create an entry in the daily-note for tomorrow.
 
 With numeric argument N, use the daily-note N days in the future.
@@ -154,40 +154,40 @@ With numeric argument N, use the daily-note N days in the future.
 With a `C-u' prefix or when GOTO is non-nil, go the note without
 creating an entry."
   (interactive "p")
-  (org-roam-dailies--capture (time-add (* n 86400) (current-time)) goto))
+  (org-roam-dailies--capture (time-add (* n 86400) (current-time)) goto keys))
 
 ;;;###autoload
-(defun org-roam-dailies-goto-tomorrow (n)
+(defun org-roam-dailies-goto-tomorrow (n keys)
   "Find the daily-note for tomorrow, creating it if necessary.
 
 With numeric argument N, use the daily-note N days in the
 future."
   (interactive "p")
-  (org-roam-dailies-capture-tomorrow n t))
+  (org-roam-dailies-capture-tomorrow n t keys))
 
 ;;;; Yesterday
 ;;;###autoload
-(defun org-roam-dailies-capture-yesterday (n &optional goto)
+(defun org-roam-dailies-capture-yesterday (n &optional goto keys)
   "Create an entry in the daily-note for yesteday.
 
 With numeric argument N, use the daily-note N days in the past.
 
 When GOTO is non-nil, go the note without creating an entry."
   (interactive "p")
-  (org-roam-dailies-capture-tomorrow (- n) goto))
+  (org-roam-dailies-capture-tomorrow (- n) goto keys))
 
 ;;;###autoload
-(defun org-roam-dailies-goto-yesterday (n)
+(defun org-roam-dailies-goto-yesterday (n keys)
   "Find the daily-note for yesterday, creating it if necessary.
 
 With numeric argument N, use the daily-note N days in the
 future."
   (interactive "p")
-  (org-roam-dailies-capture-tomorrow (- n) t))
+  (org-roam-dailies-capture-tomorrow (- n) t keys))
 
 ;;;; Date
 ;;;###autoload
-(defun org-roam-dailies-capture-date (&optional goto prefer-future)
+(defun org-roam-dailies-capture-date (&optional goto prefer-future keys)
   "Create an entry in the daily-note for a date using the calendar.
 Prefer past dates, unless PREFER-FUTURE is non-nil.
 With a `C-u' prefix or when GOTO is non-nil, go the note without
@@ -197,14 +197,14 @@ creating an entry."
                 (org-read-date nil t nil (if goto
                                              "Find daily-note: "
                                            "Capture to daily-note: ")))))
-    (org-roam-dailies--capture time goto)))
+    (org-roam-dailies--capture time goto keys)))
 
 ;;;###autoload
-(defun org-roam-dailies-goto-date (&optional prefer-future)
+(defun org-roam-dailies-goto-date (&optional prefer-future keys)
   "Find the daily-note for a date using the calendar, creating it if necessary.
 Prefer past dates, unless PREFER-FUTURE is non-nil."
   (interactive)
-  (org-roam-dailies-capture-date t prefer-future))
+  (org-roam-dailies-capture-date t prefer-future keys))
 
 ;;;; Navigation
 (defun org-roam-dailies-goto-next-note (&optional n)
