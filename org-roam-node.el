8ec2e59e67a2e39fda9328c85066e134d5aad324
Ralf Doering <ralf@rdoering.net>
2022-05-12 08:34:21 +0200

    Fix undefined incf.
    Calling org-roam-extract-subtree failes with "Symbol’s function definition is void: incf"
    
    org-roam.el includes cl-lib. org-roam--h1-count uses a bare incf call, which is undefined. Fix this by using cl-incf.

--- org-roam-node.el
+++ org-roam-node.el
@@ -837,7 +837,7 @@ Any top level properties drawers are incorporated into the new heading."
     (org-with-wide-buffer
      (org-map-region (lambda ()
                        (if (= (org-current-level) 1)
-                           (incf h1-count)))
+                           (cl-incf h1-count)))
                      (point-min) (point-max))
      h1-count)))
 
