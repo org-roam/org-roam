cef097437f082fbcf60ca463834e0103f5a896e8
notevenaperson <66701832+notevenaperson@users.noreply.github.com>
2022-06-21 22:03:12 +0000

    forgor 💀 :group

--- org-roam.el
+++ org-roam.el
@@ -109,7 +109,8 @@
 
 (defface org-roam-link-face
   `((t :inherit org-link)))
-  "Customizable face to make id: links stand out from other org links")
+  "Customizable face to make id: links stand out from other org links"
+  :group 'org-roam-faces)
 (org-link-set-parameters "id" :face 'org-roam-link-face)
 
 (defcustom org-roam-verbose t
