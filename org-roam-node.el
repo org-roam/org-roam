a00a527271f5bf9bd8f4f4e24a9ae582367df0ad
Liam Hupfer <liam@hpfr.net>
2023-09-02 11:30:54 -0500

    node: allow all diacritical marks in slugs
    2d586516999d ("optimize filename normalize (#1460)") limited the removal
    of nonspacing characters to combining diacritical marks targeting the
    Latin alphabet because removing marks for other alphabets results in
    undesirable changes to the semantics of the filename.
    
    Even for the Latin alphabet, though, removing these accents is not
    necessarily desirable and can change meaning. If users can type
    diacritical marks for their language into their node titles, they can
    likely type the same marks in a terminal emulator. Furthermore,
    multilingual users will still end up with non-ASCII-compatible filenames
    if their node titles happen to include letters from other languages.
    
    Therefore, be consistent across alphabets by default, and preserve all
    alphanumeric Unicode characters in filenames, regardless of whether
    those characters happen to be Latin-based. `string-glyph-compose` is
    retained, so NFC normalization still happens. While we’re at it, tidy
    the function as a whole.
    
    Link: https://github.com/org-roam/org-roam/pull/1460

--- org-roam-node.el
+++ org-roam-node.el
@@ -199,42 +199,11 @@ This path is relative to `org-roam-directory'."
 
 (cl-defmethod org-roam-node-slug ((node org-roam-node))
   "Return the slug of NODE."
-  (let ((title (org-roam-node-title node))
-        (slug-trim-chars '(;; Combining Diacritical Marks https://www.unicode.org/charts/PDF/U0300.pdf
-                           768 ; U+0300 COMBINING GRAVE ACCENT
-                           769 ; U+0301 COMBINING ACUTE ACCENT
-                           770 ; U+0302 COMBINING CIRCUMFLEX ACCENT
-                           771 ; U+0303 COMBINING TILDE
-                           772 ; U+0304 COMBINING MACRON
-                           774 ; U+0306 COMBINING BREVE
-                           775 ; U+0307 COMBINING DOT ABOVE
-                           776 ; U+0308 COMBINING DIAERESIS
-                           777 ; U+0309 COMBINING HOOK ABOVE
-                           778 ; U+030A COMBINING RING ABOVE
-                           779 ; U+030B COMBINING DOUBLE ACUTE ACCENT
-                           780 ; U+030C COMBINING CARON
-                           795 ; U+031B COMBINING HORN
-                           803 ; U+0323 COMBINING DOT BELOW
-                           804 ; U+0324 COMBINING DIAERESIS BELOW
-                           805 ; U+0325 COMBINING RING BELOW
-                           807 ; U+0327 COMBINING CEDILLA
-                           813 ; U+032D COMBINING CIRCUMFLEX ACCENT BELOW
-                           814 ; U+032E COMBINING BREVE BELOW
-                           816 ; U+0330 COMBINING TILDE BELOW
-                           817 ; U+0331 COMBINING MACRON BELOW
-                           )))
-    (cl-flet* ((nonspacing-mark-p (char) (memq char slug-trim-chars))
-               (strip-nonspacing-marks (s) (string-glyph-compose
-                                            (apply #'string
-                                                   (seq-remove #'nonspacing-mark-p
-                                                               (string-glyph-decompose s)))))
-               (cl-replace (title pair) (replace-regexp-in-string (car pair) (cdr pair) title)))
-      (let* ((pairs `(("[^[:alnum:][:digit:]]" . "_") ;; convert anything not alphanumeric
-                      ("__*" . "_")                   ;; remove sequential underscores
-                      ("^_" . "")                     ;; remove starting underscore
-                      ("_$" . "")))                   ;; remove ending underscore
-             (slug (-reduce-from #'cl-replace (strip-nonspacing-marks title) pairs)))
-        (downcase slug)))))
+  (thread-last (org-roam-node-title node)
+               string-glyph-compose
+               (replace-regexp-in-string (rx (1+ (not alnum))) "_")
+               (funcall (lambda (s) (string-trim s "_" "_")))
+               downcase))
 
 (cl-defmethod org-roam-node-formatted ((node org-roam-node))
   "Return a formatted string for NODE."
