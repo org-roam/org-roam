ef330a1116d58776eea98dab61ca58d5eda131ff
Martin Edström <meedstrom@runbox.eu>
2025-11-02 12:47:43 +0100

    build: add compat.el
    This should pave the way for PRs with nicer code.

--- org-roam.el
+++ org-roam.el
@@ -6,7 +6,7 @@
 ;; URL: https://github.com/org-roam/org-roam
 ;; Keywords: org-mode, roam, convenience
 ;; Version: 2.3.1
-;; Package-Requires: ((emacs "26.1") (dash "2.13") (org "9.6") (emacsql "4.1.0") (magit-section "3.0.0"))
+;; Package-Requires: ((emacs "26.1") (compat "30.1") (dash "2.13") (org "9.6") (emacsql "4.1.0") (magit-section "3.0.0"))
 
 ;; This file is NOT part of GNU Emacs.
 
@@ -76,6 +76,8 @@
 (require 'rx)
 (require 'seq)
 (require 'cl-lib)
+
+(require 'compat)
 
 (require 'magit-section)
 
